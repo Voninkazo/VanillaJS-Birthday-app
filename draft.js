aeb2c385450840cb0c93ac943b78be40db131971
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-09-18 09:04:32 +0300

    repeated

--- draft.js
+++ draft.js
@@ -12,7 +12,7 @@ async function fetchPeople() {
     const data = await response.json();
     // return data;
     let myPeople = [];
-    myPeople = data;
+    myPeople.push(data);
 
     function mirrorLocalStorage() {
         console.info('Saving items to LS');
@@ -20,7 +20,7 @@ async function fetchPeople() {
     };
 
     function storeFromLocalStorage() {
-        const listItem = JSON.parse(localStorage.getItem(myPeople));
+        const listItem = JSON.parse(localStorage.getItem('myPeople'));
         if (listItem) {
             myPeople = listItem;
             displayPeople(myPeople);
@@ -30,35 +30,38 @@ async function fetchPeople() {
         // parent.dispatchEvent(new CustomEvent('itemsUpdated'));
     }
 
-
     async function displayPeople() {
-        // const sortedPeople = myPeople.sort((a, b) => a.birthday - b.birthday);
+        const sortedPeople = myPeople.sort((a, b) => a.birthday - b.birthday);
         console.log(myPeople);
-        const html = myPeople
-            .sort((a, b) => a.birthday - b.birthday)
-            .map(people => `
-            <tr data-id="${people.id}">
-            <td class="image">
-                <img src="${people.picture}" alt="photo">
-            </td>
-            <td class="name">
-                ${people.lastName} ${people.firstName}<br>
-                <span>Turn</span>
-            </td>
-            <td class="days-left">${people.birthday}</td>
-            <td>
-                <button class="edit" value="${people.id}">
-                    <svg viewBox="0 0 20 20" fill="currentColor" class="pencil w-6 h-6"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
-                </button>
-                <button class="delete" value="${people.id}">
-                    <svg viewBox="0 0 20 20" fill="currentColor" class="trash w-6 h-6"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
-                </button>
-            </td>
-        </tr>`).join(" ");
-
+        const html = sortedPeople
+            .map(people => {
+                return `
+                <tr data-id="${people.id}">
+                    <td class="image">
+                        <img src="${people.picture}" alt="photo">
+                    </td>
+                    <td class="name">
+                        ${people.lastName} ${people.firstName}<br>
+                        <span>Turn</span>
+                    </td>
+                    <td class="days-left">${people.birthday}</td>
+                    <td>
+                        <button class="edit" value="${people.id}">
+                            <svg viewBox="0 0 20 20" fill="currentColor" class="pencil w-6 h-6"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
+                        </button>
+                        <button class="delete" value="${people.id}">
+                            <svg viewBox="0 0 20 20" fill="currentColor" class="trash w-6 h-6"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
+                        </button>
+                    </td>
+                </tr>
+            `
+            }).join(' ');
         container.innerHTML = html;
         container.dispatchEvent(new CustomEvent('itemsUpdated'));
     }
+
+    displayPeople()
+        // destroy popup
     async function destroyPopup(popup) {
         popup.classList.remove('open');
         popup.remove();
@@ -78,6 +81,7 @@ async function fetchPeople() {
     // show popup and edit data
     async function editPeoplePopup(idToEdit) {
         return new Promise(async function(resolve) {
+
             const popup = document.createElement('form');
             let personToEdit = myPeople.find(peop => peop.id === idToEdit);
             // popup edit= form
@@ -240,14 +244,13 @@ async function fetchPeople() {
             resolve();
             parent.appendChild(myForm);
             myForm.classList.add('open');
-        });
+        })
     }
 
     addBtn.addEventListener('click', addingPeople);
     window.addEventListener('click', deletePerson);
+    displayPeople(myPeople);
     container.addEventListener('itemsUpdated', mirrorLocalStorage);
     storeFromLocalStorage();
-    displayPeople()
-        // destroy popup
 }
 fetchPeople();
