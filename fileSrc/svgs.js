30f98553360a83eb6b05aefb44a1044e5a5c6f22
Sandy <sandy.rav@onja.org>
2021-03-27 14:57:13 +0300

    made improvement on styling

--- fileSrc/svgs.js
+++ fileSrc/svgs.js
@@ -1,5 +1,18 @@
-export const deleteIcon = `<svg class="w-6 h-6" fill="none" fill="red" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/200/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"></path></svg>
+export const deleteIcon = `<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M26.25 5H10L1.25 15L10 25H26.25C26.913 25 27.5489 24.7366 28.0178 24.2678C28.4866 23.7989 28.75 23.163 28.75 22.5V7.5C28.75 6.83696 28.4866 6.20107 28.0178 5.73223C27.5489 5.26339 26.913 5 26.25 5V5Z" stroke="#EF4565" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M22.5 11.25L15 18.75" stroke="#EF4565" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M15 11.25L22.5 18.75" stroke="#EF4565" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
 `;
-export const editIcon = `<svg class="w-6 h-6" fill="none" fill="blue" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/200/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>`;
-export const iconClose = `<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z" fill="#000" fill-rule="evenodd"/></svg>
+export const editIcon = `<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.75 4H4C3.33696 4 2.70107 4.26339 2.23223 4.73223C1.76339 5.20107 1.5 5.83696 1.5 6.5V24C1.5 24.663 1.76339 25.2989 2.23223 25.7678C2.70107 26.2366 3.33696 26.5 4 26.5H21.5C22.163 26.5 22.7989 26.2366 23.2678 25.7678C23.7366 25.2989 24 24.663 24 24V15.25" stroke="#094067" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M22.125 2.12499C22.6223 1.62771 23.2967 1.34834 24 1.34834C24.7033 1.34834 25.3777 1.62771 25.875 2.12499C26.3723 2.62227 26.6517 3.29673 26.6517 3.99999C26.6517 4.70325 26.3723 5.37771 25.875 5.87499L14 17.75L9 19L10.25 14L22.125 2.12499Z" stroke="#094067" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+export const iconClose = `<svg width="58" height="58" viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M43.5 14.5L14.5 43.5" stroke="#094067" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M14.5 14.5L43.5 43.5" stroke="#094067" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
 `
